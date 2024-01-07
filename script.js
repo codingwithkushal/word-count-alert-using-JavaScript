8254e21c835ec873265cb3f2040f3a5d505e384c
Kushal Joshi <143593709+codingwithkushal@users.noreply.github.com>
2024-01-07 16:01:34 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,7 @@
+function myFunction() {
+    let words = prompt("Compose your message");
+    let count = words.length;
+alert(`You have written ${count} words. You have ${150 - count} words remaining.`);
+  }
+
+  
